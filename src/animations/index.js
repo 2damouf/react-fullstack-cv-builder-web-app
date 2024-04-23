906598e2b0fba39f02193e1462b9f5e01b907732
Anıl <hasim.anil95@gmail.com>
2024-04-23 20:02:11 +0300

    new animations

--- src/animations/index.js
+++ src/animations/index.js
@@ -7,5 +7,17 @@ export const slideProfileMenuAnimation = {
 export const loginButtonAnimation = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
-  exit: { opacity: 1 },
+  exit: { opacity: 1 }
+};
+
+export const clearFilterAnimation = {
+  initial:{ opacity: 0, scale: 0.6, y: 20 },
+  animate:{ opacity: 1, scale: 1, y: 0 },
+  exit:{ opacity: 0, scale: 0.6, y: 20 }
+};
+
+export const searchCloseButtonAnimation = {
+  initial:{ opacity: 0, scale: 0.6, x: 20 },
+  animate:{ opacity: 1, scale: 1, x: 0 },
+  exit:{ opacity: 0, scale: 0.6, x: 20 }
 };
