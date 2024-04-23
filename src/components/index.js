74d9ecb58bcec40d2a8dad8fd5c24f77b1e2df5c
Anıl <hasim.anil95@gmail.com>
2024-04-23 20:03:52 +0300

    Filter and some UI Changes

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {default as AuthButton} from "./AuthButton";
 export {default as Typewriter} from "./Typewriter";
 export {default as MainLoader} from "./MainLoader";
 export {default as Header} from "./Header";
+export {default as Filter} from "./Filter";
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,6 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar')],
 }
 
